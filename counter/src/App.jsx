39e003c123165d8864305281d56c0a93d6ab5555
EmiDip <di.palma.emilie@outlook.fr>
2023-11-22 09:57:57 +0100

    counter

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -2,7 +2,52 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  const [count, setCount] = useState(0);
+  const handleClick = (action) => {
+    if (action === "increment") {
+      setCount(count + 1);
+    } else if (action === "decrement") {
+      setCount(count - 1);
+    } else if (action === "decrementFive") {
+      setCount(count - 5);
+    } else if (action === "incrementFive") {
+      setCount(count + 5);
+    }
+  };
+
+  return (
+    <div>
+      <div>{count}</div>
+      <button
+        onClick={() => {
+          handleClick("increment");
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          handleClick("decrement");
+        }}
+      >
+        -
+      </button>
+      <button
+        onClick={() => {
+          handleClick("incrementFive");
+        }}
+      >
+        +5
+      </button>
+      <button
+        onClick={() => {
+          handleClick("decrementFive");
+        }}
+      >
+        -5
+      </button>
+    </div>
+  );
 }
 
 export default App;
